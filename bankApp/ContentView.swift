e6cc2c9b98ff8fa3b1c80241e743dd389fabe263
eddyboon <66462284+eddyboon@users.noreply.github.com>
2024-04-30 12:34:37 +1000

    Initial Commit

---
+++ bankApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bankApp
+//
+//  Created by Edward Ong on 30/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bankApp/bankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  bankAppApp.swift
+//  bankApp
+//
+//  Created by Edward Ong on 30/4/24.
+//
+
+import SwiftUI
+
+@main
+struct bankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
